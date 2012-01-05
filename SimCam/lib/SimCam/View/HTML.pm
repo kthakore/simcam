03514908fd9779779b566c95be8e0911df667f28
Kartik Thakore <kthakore@localhost.(none)>
2012-01-05 13:31:02 -0500

    Made first box work

---
+++ SimCam/lib/SimCam/View/HTML.pm
@@ -0,0 +1,23 @@
+package SimCam::View::HTML;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::Xslate';
+
+__PACKAGE__->config(
+    template_extension => '.tx',
+);
+
+1;
+
+=head1 NAME
+
+SimCam::View::HTML - Xslate View for SimCam
+
+=head1 DESCRIPTION
+
+Xslate View for SimCam.
+
+=cut
+
