049f2895412fb7f186ee3355a8bc935fb355b657
Kartik Thakore <thakore.kartik@gmail.com>
2012-07-27 20:11:56 -0400

    Added process answers

--- sim_cam/lib/SimCam/Matlab.pm
+++ sim_cam/lib/SimCam/Matlab.pm
@@ -1,5 +1,34 @@
 package SimCam::Matlab;
 use IO::File;
+use Capture::Tiny qw/capture/;
+use Data::Dumper;
+use Devel::Peek;
+
+sub process_ans  {
+    my @result = @_;
+
+    my $ans = '';
+    my $flag = 0;
+
+    my $ans; my $length = scalar @result; 
+        my @results;
+
+    foreach my $l (  0..$length )
+    {
+       my $i = $length - $l;
+
+        my $line = $result[$i];
+        unless( $line =~ /^ans/ )
+        {
+           push( @results,  $line );
+        }
+        else {
+            return join(" ", reverse( @results ) );
+        }
+    }    
+}
+
+
 
 sub run_matlab {
 
@@ -8,11 +37,13 @@ sub run_matlab {
     my $matlab = 'matlab';
 
     my $run = "cd /tmp; $matlab -nodesktop -nosplash -r $script";
-    warn $run;
-
     my $result = `$run`;
 
-    warn $foo;    
+    my ( $stdout, $stderr, @result) = capture {
+        `$run`;
+    };
+
+    warn 'Ans: '. process_ans( @result );
 }
 
 sub run_matlab_script {
@@ -22,7 +53,7 @@ sub run_matlab_script {
     my $fh = IO::File->new( "> /tmp/script.m" );
 
     if( defined $fh ) {
-        print $fh " 1 + 1\nexit";
+        print $fh " [ 1 1 ]\nexit";
         $fh->close;
     } 
     run_matlab( 'script' );
