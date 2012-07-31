a8927ce3dac157201b49476a5a01014fae5463d7
Kartik Thakore <thakore.kartik@gmail.com>
2012-07-31 15:11:35 -0400

    Doing noise too

--- sim_cam/lib/SimCam/Environ.pm
+++ sim_cam/lib/SimCam/Environ.pm
@@ -1,6 +1,12 @@
 package SimCam::Environ;
 use Mojo::Base 'Mojolicious::Controller';
-use Digest::SHA qw/sha_hex/;
+use Digest::SHA qw/sha1_hex/;
+use Capture::Tiny qw/:all/;
+use MIME::Base64;
+use File::Slurp;
+use JSON;
+use XML::Simple;
+
 
 sub noise {
     my $self = shift;
@@ -10,18 +16,60 @@ sub noise {
 
 sub noiseid {
     my $self  = shift;
+    my $params = $self->req->json;
     my $alpha = $self->param('alpha');
     my $type  = $self->param('type');
 
     $self->render({ json => { alpha => $alpha, type => $type } } );
 }
 
+
 sub distort {
     my $self = shift;
     my $params = $self->req->json;
 
-    my $t1= $patams->{t1} || 0.0;
-    my $t2= $patams->{t2} || 0.0;
+     
+    $self->_run_distort( $params );
+    
+
+}
+
+
+
+sub _run_noise {
+    my $self = shift;
+    my ( $params ) = shift;
+    
+    my $image = $params->{image};
+    my $alpha = $params->{alpha} || 0.00;
+    my $type = $params->{type};
+    my $job_id = sha1_hex( localtime. $alpha. $type );
+
+    my $out = 'public/job_images/'.$job_id.'_noise.png';
+
+    my( $stdout, $stderr, @result) = capture {
+        `simcamCV/noise $image $alpha $type $out`
+    };
+
+    if( -f $out )
+    {
+        return { success => 1, job_id => $job_id, out => $out };        
+    }
+    else
+    {   
+
+        return { success => 0, job_id => $job_id, result => { out => $stdout, err => $stderr, res => \@result} };
+
+    }
+
+}
+
+sub _run_distort {
+    my $self = shift;
+    my ( $params ) = shift;
+
+    my $t1= $params->{t1} || 0.0;
+    my $t2= $params->{t2} || 0.0;
     my $r1= $params->{r1} || 0.0;
     my $r2= $params->{r2} || 0.0;
     my $r3= $params->{r3} || 0.0;
@@ -42,6 +90,8 @@ sub distort {
     my $cy = $params->{cy} || 300;
     my $cx = $params->{cx} || 240;
 
+    my $image = $params->{image} || 'in.png';
+
 
     my $Intrinsics = '<?xml version="1.0"?>
         <opencv_storage>
@@ -59,25 +109,24 @@ sub distort {
     my $dist_path = '/tmp/'.$job_id.'_dist.xml';
     my $int_path = '/tmp/'.$job_id.'_int.xml';
 
-    my $dist_fh = IO::File->new("> $dist_path" );
+    write_file( $dist_path, $Distortion );
+    write_file( $int_path, $Intrinsics );
 
-    if( defined $dist_fh )
+   
+    my $out_image = 'public/job_images/'.$job_id.'.png';
+
+    my( $stdout, $stderr, @result) = capture {
+        `simcamCV/distort $dist_path $image $out_image`
+    };
+
+    if( -f $out_image )
     {
-        print $dist_fh $Distortion;
-        $dist_fh->close();
+        return { success => 1, out => $out_image, job_id => $job_id };
     }
-
-    my $int_fh = IO::File->new("> $int_path" );
-
-    if( defined $int_fh )
+    else
     {
-        print $int_fh $Intrinsics;
-        $int_fh->close();
+        return { success => 0, result => { out => $stdout, err => $stderr, res => \@result } , job_id => $job_id};  
     }
-
-    
-
 }
-
 
 1;
