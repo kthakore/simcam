a67330a158161575b2f162ee381fd8f7de7a59f6
Kartik Thakore <kthakore@uwo.ca>
2013-02-26 17:48:01 -0500

    Added in main view

--- sim_cam/public/js/environment.js
+++ sim_cam/public/js/environment.js
@@ -23,7 +23,7 @@ SimCam.Constructor.Model.Generic = Backbone.Model.extend({});
 SimCam.Template.MainFrame = '<iframe src="/iframes/environment.html" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0" class="simcam_iframe" width="100%" height="640px" ></iframe>';
 
 SimCam.Template.SideMenu = {
-    "Pinhole" : '<h5>Pinhole Camera Model</h5>' +
+    "pinhole" : '<h5>Pinhole Camera Model</h5>' +
                 '<li class="nav-header">Field of View</li>' +
                 '<li><a href="javascript:void(0)"><input type="text" name="fov" class="span1 pinhole_sidemenu_input"/></a></li>' +
                 '<li class="nav-header">Sensor Size in Pixels</li>' +
@@ -31,16 +31,285 @@ SimCam.Template.SideMenu = {
                 '<li><a href="javascript:void(0)">Y: <input type="text" name="v" class="span1 pinhole_sidemenu_input"/></a></li>' +
                 '<li class="nav-header">Aspect Ratio</li>' +
                 '<li><a href="javascript:void(0)"><input type="text" name="aspect_ratio" class="span1 pinhole_sidemenu_input"/></a></li>' +
-                '<li style="width=100%; height=100%; background-color:red"></li>'
-
+                '<li><input type="button" class="btn btn-primary pinhole_sidemenu_apply_btn" value="Apply" /></li>',
+    "matrix" : '<h5>Apply Matrix</h5>' +
+                '<li><input type="button" class="btn btn-primary matrix_sidemenu_apply_btn" value="Apply" /></li>'
 
 };
 
 
 /*View Constructors*/
 SimCam.Constructor.View.MainCanvas = Backbone.View.extend({
-    initialize: function (options) {
-        "use strict";
+    initialize: function() {
+        var that = this;
+                this.mouse =  new THREE.Vector2(), this.offset = new THREE.Vector3();
+
+  				this.camera = new THREE.PerspectiveCamera( 35, $(this.el).innerWidth() / window.innerHeight, 1, 10000 );
+                this.camera.position.set(65,65,65);
+
+				this.controls = new THREE.OrbitControls( this.camera );
+				this.controls.rotateSpeed = 1.0;
+				this.controls.zoomSpeed = 1.2;
+				this.controls.panSpeed = 0.8;
+				this.controls.noZoom = false;
+				this.controls.noPan = false;
+				this.controls.staticMoving = true;
+				this.controls.dynamicDampingFactor = 0.3;
+
+				this.scene = new THREE.Scene();
+                this.objects = $([]);
+
+				this.scene.add( new THREE.AmbientLight( 0x505050 ) );
+
+				var light = new THREE.SpotLight( 0xffffff, 1.5 );
+				light.position.set( 0, 500, 2000 );
+				light.castShadow = true;
+
+				light.shadowCameraNear = 200;
+				light.shadowCameraFar = this.camera.far;
+				light.shadowCameraFov = 50;
+
+				light.shadowBias = -0.00022;
+				light.shadowDarkness = 0.5;
+
+				light.shadowMapWidth = 2048;
+				light.shadowMapHeight = 2048;
+                this.lights = $([]);
+                this.lights.push( light );
+
+				this.scene.add( light );
+
+                var jsonLoader = new THREE.JSONLoader();
+                jsonLoader.load( "/3d_objs/camera.js", function( geometry ) {  
+
+                    var cam_obj = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: 0x2200cc } ) );
+
+					cam_obj.material.ambient = cam_obj.material.color;
+
+					cam_obj.position.set(0,0,15);
+
+					cam_obj.scale.set(1,1,1);
+                    cam_obj.rotation.set( 0 ,0,  0 );
+                    cam_obj.model = that.options.models.camera;
+//					cam_obj.castShadow = false;
+//					cam_obj.receiveShadow = false;
+            var material = new THREE.MeshLambertMaterial({
+            map: THREE.ImageUtils.loadTexture("img/grid.gif")
+            });
+
+            material.map.needsUpdate = true;
+
+            var cube = new THREE.Mesh(
+                    new THREE.CubeGeometry( 8, 5, 0.1 ),
+                    material
+                    );
+            cube.model = that.options.models.grid;
+            cube.model.trigger( 'init', cube.model, cube ); 
+            cube.overdraw = true;
+           that.scene.add( cube ); that.objects.push( cube );
+            
+            cube.position.set(0,0,0);
+
+					that.scene.add( cam_obj );
+
+					that.objects.push( cam_obj );
+            cam_obj.model.trigger( 'init', cam_obj.model, cam_obj ); 
+
+
+                    } );
+                    
+				this.plane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4), new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.25, transparent: true, wireframe: true } ) );
+				xplane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4 ), new THREE.MeshBasicMaterial( { color: 0xCC0000, opacity: 0.25, transparent: true, wireframe: true } ) );
+				yplane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4 ), new THREE.MeshBasicMaterial( { color: 0x00CC00, opacity: 0.25, transparent: true, wireframe: true } ) );
+				zplane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4 ), new THREE.MeshBasicMaterial( { color: 0x0000CC, opacity: 0.25, transparent: true, wireframe: true } ) );
+
+                yplane.rotation.set( 0, 1.57079633, 0 );
+                zplane.rotation.set( 1.57079633, 0, 0 );
+
+
+                this.scene.add(xplane); this.scene.add(yplane); this.scene.add(zplane);
+
+				this.plane.visible = false;
+				this.scene.add(this.plane );
+
+				this.projector = new THREE.Projector();
+
+				this.renderer = new THREE.WebGLRenderer( { antialias: true, clearColor: 0x888888, clearAlpha: 255  } );
+				this.renderer.sortObjects = false;
+				this.renderer.setSize( $(this.el).innerWidth(), window.innerHeight );
+
+				this.renderer.shadowMapEnabled = true;
+				this.renderer.shadowMapSoft = true;
+
+				this.el.appendChild(this.renderer.domElement );
+
+				/*
+				renderer.domElement.addEventListener( 'mousemove', onDocumentMouseMove, false );
+				renderer.domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
+				renderer.domElement.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+
+				window.addEventListener( 'resize', onWindowResize, false );
+                */
+                $(window).on('resize', function() { that.on_resize(); } )
+                this.animate();
+    },
+    events : {
+       'mousemove canvas' : 'on_canvas_mmove',
+       'mousedown canvas' : 'on_canvas_mdown',
+       'mouseup canvas' : 'on_canvas_mup',
+    },
+    on_canvas_mmove : function( event ) {
+                
+
+
+				event.preventDefault();
+
+			    this.mouse.x = ( event.clientX / $(this.el).innerWidth() ) * 2 - 1;
+				this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+				//
+
+				var vector = new THREE.Vector3( this.mouse.x, this.mouse.y, 0.5 );
+				this.projector.unprojectVector( vector, this.camera );
+
+				var ray = new THREE.Ray( this.camera.position, vector.subSelf( this.camera.position ).normalize() );
+
+
+				if ( this.SELECTED ) {
+
+					var intersects = ray.intersectObject( this.plane );
+                    if( intersects[0] && intersects[0].point ) {
+                        if( event.shiftKey )
+                        {
+                            var loc =  intersects[ 0 ].point.subSelf( this.offset ); 
+                            var vector = new THREE.Vector3(0,0,0);
+
+                            vector.sub( this.SELECTED.position, loc );
+
+                                                // Direction we are already facing (without rotation)
+                    var forward = new THREE.Vector3(0,0,-1);
+
+                    // Direction we want to be facing (towards mouse pointer)
+                    var target = vector.normalize();
+
+                    // Axis and angle of rotation
+                    var axis = new THREE.Vector3().cross(forward, target);
+                    var sinAngle = axis.length(); // |u x v| = |u|*|v|*sin(a)
+                    var cosAngle = forward.dot(target); // u . v = |u|*|v|*cos(a)
+                    var angle = Math.atan2(sinAngle, cosAngle); // atan2(sin(a),cos(a)) = a
+                    axis.normalize();
+                   
+                    var a = new THREE.Mesh();
+                    a.useQuaternion = true;
+                    a.quaternion.setFromAxisAngle(axis, angle);
+                        this.SELECTED.rotation.set( a.quaternion.x, a.quaternion.y, a.quaternion.z );
+                        this.SELECTED.rotation.multiplyScalar( 0.5 );
+       
+                        }
+                        else {
+					       this.SELECTED.position.copy( intersects[ 0 ].point.subSelf( this.offset ) );
+                        }
+                        this.SELECTED.model.trigger( 'move', this.SELECTED.model, this.SELECTED );
+                    }
+					return;
+
+				}
+
+
+				var intersects = ray.intersectObjects( this.objects );
+
+				if ( intersects.length > 0 ) {
+
+					if ( this.INTERSECTED != intersects[ 0 ].object ) {
+
+						if ( this.INTERSECTED ) this.INTERSECTED.material.color.setHex( this.INTERSECTED.currentHex );
+
+						this.INTERSECTED = intersects[ 0 ].object;
+						this.INTERSECTED.currentHex = this.INTERSECTED.material.color.getHex();
+
+						this.plane.position.copy( this.INTERSECTED.position );
+						this.plane.lookAt( this.camera.position );
+                        this.plane.rotation.copy( this.camera.rotation );
+
+					}
+
+				    this.el.style.cursor = 'pointer';
+
+				} else {
+
+					if ( this.INTERSECTED ) this.INTERSECTED.material.color.setHex( this.INTERSECTED.currentHex );
+
+					this.INTERSECTED = null;
+
+					this.el.style.cursor = 'auto';
+
+				}
+
+
+    },
+    on_canvas_mdown : function(event ) {
+
+				event.preventDefault();
+
+				var vector = new THREE.Vector3( this.mouse.x, this.mouse.y, 0.5 );
+				this.projector.unprojectVector( vector, this.camera );
+
+				var ray = new THREE.Ray( this.camera.position, vector.subSelf( this.camera.position ).normalize() );
+
+				var intersects = ray.intersectObjects( this.objects );
+
+				if ( intersects.length > 0 ) {
+
+					this.controls.enabled = false;
+
+					this.SELECTED = intersects[ 0 ].object;
+
+					var intersects = ray.intersectObject( this.plane );
+					this.offset.copy( intersects[ 0 ].point ).subSelf( this.plane.position );
+
+					this.el.style.cursor = 'move';
+
+				}
+
+
+    },
+    on_canvas_mup : function(event ) {
+				event.preventDefault();
+
+				this.controls.enabled = true;
+
+				if ( this.INTERSECTED ) {
+
+					this.plane.position.copy( this.INTERSECTED.position );
+
+					this.SELECTED = null;
+
+				}
+
+				this.el.style.cursor = 'auto';
+
+    },
+    on_resize : function (event ) {
+
+				this.camera.aspect = $(this.el).innerWidth() / window.innerHeight;
+				this.camera.updateProjectionMatrix();
+
+				this.renderer.setSize( $(this.el).innerWidth(), window.innerHeight );
+
+
+    },
+    animate : function () {
+                var that = this; 
+				requestAnimationFrame( function() { that.animate() } );
+				this.render();
+    }, 
+    render : function () {
+
+				this.controls.update();
+
+				this.renderer.render( this.scene, this.camera );
+
 
     }
 });
@@ -66,21 +335,12 @@ SimCam.Constructor.View.SideMenu = Backbone.View.extend({
     },
     render: function (mode) {
         "use strict";
-        var that;
-        that = this;
-        switch (mode.type) {
-        case 'pinhole':
-            that.render_pinhole();
-            break;
-        default:
-            console.error('SideMenu: Mode not implemented');
-        }
-    },
-    render_pinhole: function () {
-        "use strict";
         var that, template;
         that = this;
-        template = SimCam.Template.SideMenu.Pinhole;
+        template = SimCam.Template.SideMenu[mode.type];
+        if (template === undefined) {
+            template = 'Sidemenu template not implemented for: ' + mode.type;
+        }
         that.$('.body').html(template);
 
     }
--- sim_cam/public/js/libs/three.js
+++ sim_cam/public/js/libs/three.js
@@ -792,3 +792,305 @@ THREE.ShaderFlares={lensFlareVertexTexture:{vertexShader:"uniform vec3 screenPos
 lensFlare:{vertexShader:"uniform vec3 screenPosition;\nuniform vec2 scale;\nuniform float rotation;\nuniform int renderType;\nattribute vec2 position;\nattribute vec2 uv;\nvarying vec2 vUV;\nvoid main() {\nvUV = uv;\nvec2 pos = position;\nif( renderType == 2 ) {\npos.x = cos( rotation ) * position.x - sin( rotation ) * position.y;\npos.y = sin( rotation ) * position.x + cos( rotation ) * position.y;\n}\ngl_Position = vec4( ( pos * scale + screenPosition.xy ).xy, screenPosition.z, 1.0 );\n}",fragmentShader:"precision mediump float;\nuniform sampler2D map;\nuniform sampler2D occlusionMap;\nuniform float opacity;\nuniform int renderType;\nuniform vec3 color;\nvarying vec2 vUV;\nvoid main() {\nif( renderType == 0 ) {\ngl_FragColor = vec4( texture2D( map, vUV ).rgb, 0.0 );\n} else if( renderType == 1 ) {\ngl_FragColor = texture2D( map, vUV );\n} else {\nfloat visibility = texture2D( occlusionMap, vec2( 0.5, 0.1 ) ).a +\ntexture2D( occlusionMap, vec2( 0.9, 0.5 ) ).a +\ntexture2D( occlusionMap, vec2( 0.5, 0.9 ) ).a +\ntexture2D( occlusionMap, vec2( 0.1, 0.5 ) ).a;\nvisibility = ( 1.0 - visibility / 4.0 );\nvec4 texture = texture2D( map, vUV );\ntexture.a *= opacity * visibility;\ngl_FragColor = texture;\ngl_FragColor.rgb *= color;\n}\n}"}};
 THREE.ShaderSprite={sprite:{vertexShader:"uniform int useScreenCoordinates;\nuniform int affectedByDistance;\nuniform vec3 screenPosition;\nuniform mat4 modelViewMatrix;\nuniform mat4 projectionMatrix;\nuniform float rotation;\nuniform vec2 scale;\nuniform vec2 alignment;\nuniform vec2 uvOffset;\nuniform vec2 uvScale;\nattribute vec2 position;\nattribute vec2 uv;\nvarying vec2 vUV;\nvoid main() {\nvUV = uvOffset + uv * uvScale;\nvec2 alignedPosition = position + alignment;\nvec2 rotatedPosition;\nrotatedPosition.x = ( cos( rotation ) * alignedPosition.x - sin( rotation ) * alignedPosition.y ) * scale.x;\nrotatedPosition.y = ( sin( rotation ) * alignedPosition.x + cos( rotation ) * alignedPosition.y ) * scale.y;\nvec4 finalPosition;\nif( useScreenCoordinates != 0 ) {\nfinalPosition = vec4( screenPosition.xy + rotatedPosition, screenPosition.z, 1.0 );\n} else {\nfinalPosition = projectionMatrix * modelViewMatrix * vec4( 0.0, 0.0, 0.0, 1.0 );\nfinalPosition.xy += rotatedPosition * ( affectedByDistance == 1 ? 1.0 : finalPosition.z );\n}\ngl_Position = finalPosition;\n}",
 fragmentShader:"precision mediump float;\nuniform vec3 color;\nuniform sampler2D map;\nuniform float opacity;\nvarying vec2 vUV;\nvoid main() {\nvec4 texture = texture2D( map, vUV );\ngl_FragColor = vec4( color * texture.xyz, texture.a * opacity );\n}"}};
+
+THREE.OrbitControls = function ( object, domElement ) {
+
+	THREE.EventDispatcher.call( this );
+
+	this.object = object;
+	this.domElement = ( domElement !== undefined ) ? domElement : document;
+
+	// API
+
+	this.center = new THREE.Vector3();
+
+	this.userZoom = true;
+	this.userZoomSpeed = 1.0;
+
+	this.userRotate = true;
+	this.userRotateSpeed = 1.0;
+
+	this.autoRotate = false;
+	this.autoRotateSpeed = 2.0; // 30 seconds per round when fps is 60
+
+	this.minPolarAngle = 0; // radians
+	this.maxPolarAngle = Math.PI; // radians
+
+	this.minDistance = 0;
+	this.maxDistance = Infinity;
+
+	// internals
+
+	var scope = this;
+
+	var EPS = 0.000001;
+	var PIXELS_PER_ROUND = 1800;
+
+	var rotateStart = new THREE.Vector2();
+	var rotateEnd = new THREE.Vector2();
+	var rotateDelta = new THREE.Vector2();
+
+	var zoomStart = new THREE.Vector2();
+	var zoomEnd = new THREE.Vector2();
+	var zoomDelta = new THREE.Vector2();
+
+	var phiDelta = 0;
+	var thetaDelta = 0;
+	var scale = 1;
+
+	var lastPosition = new THREE.Vector3();
+
+	var STATE = { NONE : -1, ROTATE : 0, ZOOM : 1 };
+	var state = STATE.NONE;
+
+	// events
+
+	var changeEvent = { type: 'change' };
+
+
+	this.rotateLeft = function ( angle ) {
+
+		if ( angle === undefined ) {
+
+			angle = getAutoRotationAngle();
+
+		}
+
+		thetaDelta -= angle;
+
+	};
+
+	this.rotateRight = function ( angle ) {
+
+		if ( angle === undefined ) {
+
+			angle = getAutoRotationAngle();
+
+		}
+
+		thetaDelta += angle;
+
+	};
+
+	this.rotateUp = function ( angle ) {
+
+		if ( angle === undefined ) {
+
+			angle = getAutoRotationAngle();
+
+		}
+
+		phiDelta -= angle;
+
+	};
+
+	this.rotateDown = function ( angle ) {
+
+		if ( angle === undefined ) {
+
+			angle = getAutoRotationAngle();
+
+		}
+
+		phiDelta += angle;
+
+	};
+
+	this.zoomIn = function ( zoomScale ) {
+
+		if ( zoomScale === undefined ) {
+
+			zoomScale = getZoomScale();
+
+		}
+
+		scale /= zoomScale;
+
+	};
+
+	this.zoomOut = function ( zoomScale ) {
+
+		if ( zoomScale === undefined ) {
+
+			zoomScale = getZoomScale();
+
+		}
+
+		scale *= zoomScale;
+
+	};
+
+	this.update = function () {
+
+		var position = this.object.position;
+		var offset = position.clone().sub( this.center )
+
+		// angle from z-axis around y-axis
+
+		var theta = Math.atan2( offset.x, offset.z );
+
+		// angle from y-axis
+
+		var phi = Math.atan2( Math.sqrt( offset.x * offset.x + offset.z * offset.z ), offset.y );
+
+		if ( this.autoRotate ) {
+
+			this.rotateLeft( getAutoRotationAngle() );
+
+		}
+
+		theta += thetaDelta;
+		phi += phiDelta;
+
+		// restrict phi to be between desired limits
+		phi = Math.max( this.minPolarAngle, Math.min( this.maxPolarAngle, phi ) );
+
+		// restrict phi to be betwee EPS and PI-EPS
+		phi = Math.max( EPS, Math.min( Math.PI - EPS, phi ) );
+
+		var radius = offset.length() * scale;
+
+		// restrict radius to be between desired limits
+		radius = Math.max( this.minDistance, Math.min( this.maxDistance, radius ) );
+
+		offset.x = radius * Math.sin( phi ) * Math.sin( theta );
+		offset.y = radius * Math.cos( phi );
+		offset.z = radius * Math.sin( phi ) * Math.cos( theta );
+
+		position.copy( this.center ).add( offset );
+
+		this.object.lookAt( this.center );
+
+		thetaDelta = 0;
+		phiDelta = 0;
+		scale = 1;
+
+		if ( lastPosition.distanceTo( this.object.position ) > 0 ) {
+
+			this.dispatchEvent( changeEvent );
+
+			lastPosition.copy( this.object.position );
+
+		}
+
+	};
+
+
+	function getAutoRotationAngle() {
+
+		return 2 * Math.PI / 60 / 60 * scope.autoRotateSpeed;
+
+	}
+
+	function getZoomScale() {
+
+		return Math.pow( 0.95, scope.userZoomSpeed );
+
+	}
+
+	function onMouseDown( event ) {
+
+		if ( !scope.userRotate ) return;
+
+		event.preventDefault();
+
+		if ( event.button === 0 || event.button === 2 ) {
+
+			state = STATE.ROTATE;
+
+			rotateStart.set( event.clientX, event.clientY );
+
+		} else if ( event.button === 1 ) {
+
+			state = STATE.ZOOM;
+
+			zoomStart.set( event.clientX, event.clientY );
+
+		}
+
+		document.addEventListener( 'mousemove', onMouseMove, false );
+		document.addEventListener( 'mouseup', onMouseUp, false );
+
+	}
+
+	function onMouseMove( event ) {
+
+		event.preventDefault();
+
+		if ( state === STATE.ROTATE ) {
+
+			rotateEnd.set( event.clientX, event.clientY );
+			rotateDelta.subVectors( rotateEnd, rotateStart );
+
+			scope.rotateLeft( 2 * Math.PI * rotateDelta.x / PIXELS_PER_ROUND * scope.userRotateSpeed );
+			scope.rotateUp( 2 * Math.PI * rotateDelta.y / PIXELS_PER_ROUND * scope.userRotateSpeed );
+
+			rotateStart.copy( rotateEnd );
+
+		} else if ( state === STATE.ZOOM ) {
+
+			zoomEnd.set( event.clientX, event.clientY );
+			zoomDelta.subVectors( zoomEnd, zoomStart );
+
+			if ( zoomDelta.y > 0 ) {
+
+				scope.zoomIn();
+
+			} else {
+
+				scope.zoomOut();
+
+			}
+
+			zoomStart.copy( zoomEnd );
+
+		}
+
+	}
+
+	function onMouseUp( event ) {
+
+		if ( ! scope.userRotate ) return;
+
+		document.removeEventListener( 'mousemove', onMouseMove, false );
+		document.removeEventListener( 'mouseup', onMouseUp, false );
+
+		state = STATE.NONE;
+
+	}
+
+	function onMouseWheel( event ) {
+
+		if ( ! scope.userZoom ) return;
+
+		var delta = 0;
+
+		if ( event.wheelDelta ) { // WebKit / Opera / Explorer 9
+
+			delta = event.wheelDelta;
+
+		} else if ( event.detail ) { // Firefox
+
+			delta = - event.detail;
+
+		}
+
+		if ( delta > 0 ) {
+
+			scope.zoomOut();
+
+		} else {
+
+			scope.zoomIn();
+
+		}
+
+	}
+
+	this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
+	this.domElement.addEventListener( 'mousedown', onMouseDown, false );
+	this.domElement.addEventListener( 'mousewheel', onMouseWheel, false );
+	this.domElement.addEventListener( 'DOMMouseScroll', onMouseWheel, false ); // firefox
+
+};
+
