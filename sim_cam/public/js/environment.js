7d061b5043727016829c6702bfea70a59687ccde
Kartik Thakore <kthakore@uwo.ca>
2013-02-26 18:38:21 -0500

    Showing 3D with jslint problems

--- sim_cam/public/js/environment.js
+++ sim_cam/public/js/environment.js
@@ -1,7 +1,7 @@
 /*jslint browser: true*/
 /*jslint newcap: false*/
 /*jslint nomen: true */
-/*global $, _, jQuery, Backbone, console, alert */
+/*global $, _, jQuery, Backbone, console, alert, THREE */
 /*jshint globalstrict: true*/
 
 var SimCam = {
@@ -40,120 +40,123 @@ SimCam.Template.SideMenu = {
 
 /*View Constructors*/
 SimCam.Constructor.View.MainCanvas = Backbone.View.extend({
-    initialize: function() {
+    initialize: function () {
+        "use strict";
         var that = this;
-                this.mouse =  new THREE.Vector2(), this.offset = new THREE.Vector3();
+        that.mouse =  new THREE.Vector2();
+        that.offset = new THREE.Vector3();
 
-  				this.camera = new THREE.PerspectiveCamera( 35, $(this.el).innerWidth() / window.innerHeight, 1, 10000 );
-                this.camera.position.set(65,65,65);
+        this.camera = new THREE.PerspectiveCamera(35, $(this.el).innerWidth() / window.innerHeight, 1, 10000);
+        this.camera.position.set(65,65,65);
 
-				this.controls = new THREE.OrbitControls( this.camera );
-				this.controls.rotateSpeed = 1.0;
-				this.controls.zoomSpeed = 1.2;
-				this.controls.panSpeed = 0.8;
-				this.controls.noZoom = false;
-				this.controls.noPan = false;
-				this.controls.staticMoving = true;
-				this.controls.dynamicDampingFactor = 0.3;
+        this.controls = new THREE.OrbitControls( this.camera );
+        this.controls.rotateSpeed = 1.0;
+        this.controls.zoomSpeed = 1.2;
+        this.controls.panSpeed = 0.8;
+        this.controls.noZoom = false;
+        this.controls.noPan = false;
+        this.controls.staticMoving = true;
+        this.controls.dynamicDampingFactor = 0.3;
 
-				this.scene = new THREE.Scene();
-                this.objects = $([]);
+        this.scene = new THREE.Scene();
+        this.objects = $([]);
 
-				this.scene.add( new THREE.AmbientLight( 0x505050 ) );
+        this.scene.add( new THREE.AmbientLight( 0x505050 ) );
 
-				var light = new THREE.SpotLight( 0xffffff, 1.5 );
-				light.position.set( 0, 500, 2000 );
-				light.castShadow = true;
+        var light = new THREE.SpotLight( 0xffffff, 1.5 );
+        light.position.set( 0, 500, 2000 );
+        light.castShadow = true;
 
-				light.shadowCameraNear = 200;
-				light.shadowCameraFar = this.camera.far;
-				light.shadowCameraFov = 50;
+        light.shadowCameraNear = 200;
+        light.shadowCameraFar = this.camera.far;
+        light.shadowCameraFov = 50;
 
-				light.shadowBias = -0.00022;
-				light.shadowDarkness = 0.5;
+        light.shadowBias = -0.00022;
+        light.shadowDarkness = 0.5;
 
-				light.shadowMapWidth = 2048;
-				light.shadowMapHeight = 2048;
-                this.lights = $([]);
-                this.lights.push( light );
+        light.shadowMapWidth = 2048;
+        light.shadowMapHeight = 2048;
+        this.lights = $([]);
+        this.lights.push( light );
 
-				this.scene.add( light );
+        this.scene.add( light );
 
-                var jsonLoader = new THREE.JSONLoader();
-                jsonLoader.load( "/3d_objs/camera.js", function( geometry ) {  
+        var jsonLoader = new THREE.JSONLoader();
+        jsonLoader.load( "/3d_objs/camera.js", function( geometry ) {  
 
-                    var cam_obj = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: 0x2200cc } ) );
+                var cam_obj = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: 0x2200cc } ) );
 
-					cam_obj.material.ambient = cam_obj.material.color;
+                cam_obj.material.ambient = cam_obj.material.color;
 
-					cam_obj.position.set(0,0,15);
+                cam_obj.position.set(0,0,15);
 
-					cam_obj.scale.set(1,1,1);
-                    cam_obj.rotation.set( 0 ,0,  0 );
-                    cam_obj.model = that.options.models.camera;
-//					cam_obj.castShadow = false;
-//					cam_obj.receiveShadow = false;
-            var material = new THREE.MeshLambertMaterial({
-            map: THREE.ImageUtils.loadTexture("img/grid.gif")
-            });
+                cam_obj.scale.set(1,1,1);
+                cam_obj.rotation.set( 0 ,0,  0 );
+                console.log( that.options.app.models.camera );
+                cam_obj.model = that.options.app.models.camera;
+                //					cam_obj.castShadow = false;
+                //					cam_obj.receiveShadow = false;
+                var material = new THREE.MeshLambertMaterial({
+map: THREE.ImageUtils.loadTexture("img/grid.gif")
+});
 
-            material.map.needsUpdate = true;
+                material.map.needsUpdate = true;
 
-            var cube = new THREE.Mesh(
+                var cube = new THREE.Mesh(
                     new THREE.CubeGeometry( 8, 5, 0.1 ),
                     material
                     );
-            cube.model = that.options.models.grid;
-            cube.model.trigger( 'init', cube.model, cube ); 
-            cube.overdraw = true;
-           that.scene.add( cube ); that.objects.push( cube );
-            
-            cube.position.set(0,0,0);
+                cube.model = that.options.app.models.grid;
+                cube.model.trigger( 'init', cube.model, cube ); 
+                cube.overdraw = true;
+                that.scene.add( cube ); that.objects.push( cube );
 
-					that.scene.add( cam_obj );
+                cube.position.set(0,0,0);
 
-					that.objects.push( cam_obj );
-            cam_obj.model.trigger( 'init', cam_obj.model, cam_obj ); 
+                that.scene.add( cam_obj );
 
-
-                    } );
-                    
-				this.plane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4), new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.25, transparent: true, wireframe: true } ) );
-				xplane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4 ), new THREE.MeshBasicMaterial( { color: 0xCC0000, opacity: 0.25, transparent: true, wireframe: true } ) );
-				yplane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4 ), new THREE.MeshBasicMaterial( { color: 0x00CC00, opacity: 0.25, transparent: true, wireframe: true } ) );
-				zplane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4 ), new THREE.MeshBasicMaterial( { color: 0x0000CC, opacity: 0.25, transparent: true, wireframe: true } ) );
-
-                yplane.rotation.set( 0, 1.57079633, 0 );
-                zplane.rotation.set( 1.57079633, 0, 0 );
+                that.objects.push( cam_obj );
+                cam_obj.model.trigger( 'init', cam_obj.model, cam_obj ); 
 
 
-                this.scene.add(xplane); this.scene.add(yplane); this.scene.add(zplane);
+                } );
 
-				this.plane.visible = false;
-				this.scene.add(this.plane );
+this.plane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4), new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.25, transparent: true, wireframe: true } ) );
+xplane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4 ), new THREE.MeshBasicMaterial( { color: 0xCC0000, opacity: 0.25, transparent: true, wireframe: true } ) );
+yplane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4 ), new THREE.MeshBasicMaterial( { color: 0x00CC00, opacity: 0.25, transparent: true, wireframe: true } ) );
+zplane = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000, 4, 4 ), new THREE.MeshBasicMaterial( { color: 0x0000CC, opacity: 0.25, transparent: true, wireframe: true } ) );
 
-				this.projector = new THREE.Projector();
-
-				this.renderer = new THREE.WebGLRenderer( { antialias: true, clearColor: 0x888888, clearAlpha: 255  } );
-				this.renderer.sortObjects = false;
-				this.renderer.setSize( $(this.el).innerWidth(), window.innerHeight );
-
-				this.renderer.shadowMapEnabled = true;
-				this.renderer.shadowMapSoft = true;
-
-				this.el.appendChild(this.renderer.domElement );
-
-				/*
-				renderer.domElement.addEventListener( 'mousemove', onDocumentMouseMove, false );
-				renderer.domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
-				renderer.domElement.addEventListener( 'mouseup', onDocumentMouseUp, false );
+yplane.rotation.set( 0, 1.57079633, 0 );
+zplane.rotation.set( 1.57079633, 0, 0 );
 
 
-				window.addEventListener( 'resize', onWindowResize, false );
-                */
-                $(window).on('resize', function() { that.on_resize(); } )
-                this.animate();
-    },
+this.scene.add(xplane); this.scene.add(yplane); this.scene.add(zplane);
+
+this.plane.visible = false;
+this.scene.add(this.plane );
+
+this.projector = new THREE.Projector();
+
+this.renderer = new THREE.WebGLRenderer( { antialias: true, clearColor: 0x888888, clearAlpha: 255, canvas: this.$('canvas')[0]  } );
+this.renderer.sortObjects = false;
+this.renderer.setSize( $(this.el).innerWidth(), window.innerHeight );
+
+this.renderer.shadowMapEnabled = true;
+this.renderer.shadowMapSoft = true;
+
+//this.el.appendChild(this.renderer.domElement );
+
+/*
+   renderer.domElement.addEventListener( 'mousemove', onDocumentMouseMove, false );
+   renderer.domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
+   renderer.domElement.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+
+   window.addEventListener( 'resize', onWindowResize, false );
+   */
+$(window).on('resize', function() { that.on_resize(); } )
+this.animate();
+},
     events : {
        'mousemove canvas' : 'on_canvas_mmove',
        'mousedown canvas' : 'on_canvas_mdown',
@@ -428,7 +431,7 @@ SimCam.Constructor.Router.App = Backbone.Router.extend({
 
         that.element = options.element;
 
-        that.models = { camera: new SimCam.Constructor.Model.Generic() };
+        that.models = { camera: new SimCam.Constructor.Model.Generic(), grid: new SimCam.Constructor.Model.Generic() };
 
         env_frame = $(SimCam.Template.MainFrame);
 
